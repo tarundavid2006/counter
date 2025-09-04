d04cc36900d1e0ccbad94511b376821def11d8a0
tarundavid2006 <tarundavid2303@gmail.ccm>
2025-09-04 21:46:28 +0530

    Add JS

---
+++ counter.js
@@ -0,0 +1,17 @@
+const ibtn = document.getElementById("ibtn");
+const dbtn = document.getElementById("dbtn");
+const rbtn = document.getElementById("rbtn");
+const countlabel = document.getElementById("countlabel");
+let count = 0;
+ibtn.onclick = function () {
+  count++;
+  countlabel.textContent = count;
+};
+dbtn.onclick = function () {
+  count--;
+  countlabel.textContent = count;
+};
+rbtn.onclick = function () {
+  count = 0;
+  countlabel.textContent = count;
+};
